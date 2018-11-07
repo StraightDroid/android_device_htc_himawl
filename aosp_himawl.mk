9fe475317dcee858560aba9dd015c32f831c7182
StraightDroid <straightdroid@gmail.com>
2018-11-07 14:17:24 +0000

    first changes
    Change-Id: I635cf8ebd56caae39c1d46570a3c5eb39b034c08

---
+++ aosp_himawl.mk
@@ -0,0 +1,33 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
+PRODUCT_NAME := aosp_himawl
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="htc_himawl" \
+    PRODUCT_NAME="HTCOneM9vzw" \
+    PRIVATE_BUILD_DESC="4.49.605.11 CL947620 release-keys"
+
+BUILD_FINGERPRINT := "htc/HTCOneM9vzw/htc_himawl:7.0/NRD90M/947620.11:user/release-keys"
